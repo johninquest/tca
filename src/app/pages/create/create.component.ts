256d9d7601061bd566bfe5bfc7f77b7ba0db04dc
John <john-francis.edah@tech11.com>
2023-10-09 11:24:10 +0200

    improved create page

--- src/app/pages/create/create.component.ts
+++ src/app/pages/create/create.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ListDataModel } from 'src/app/utils/data.models';
-import { MainCategoryList } from 'src/app/shared/categories';
+import { MainCategoryList, AssetCategoryList, LiabilityCategoryList } from 'src/app/shared/categories';
 
 @Component({
   selector: 'app-create',
@@ -12,9 +12,22 @@ export class CreateComponent {
   constructor() { }
 
   createForm = new FormGroup({
-    createType: new FormControl<string | null>('', Validators.required),
+    accountName: new FormControl<string>('', Validators.required), 
+    categoryName: new FormControl<string>('', Validators.required),
     isUrgent: new FormControl<string | null>(''),
   });
 
   createTypeList: ListDataModel[] = MainCategoryList;
+  assetTypeList: ListDataModel[] = AssetCategoryList; 
+  liabilityTypeList: ListDataModel[] = LiabilityCategoryList;
+
+  setCategoryList(mainCategory: any) {
+    if(mainCategory === 'asset') {
+      return this.assetTypeList;
+    }if(mainCategory === 'liability') {
+      return this.liabilityTypeList;
+    }else {
+      return [];
+    }
+  }
 }
