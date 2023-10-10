903f5d14e32b9e951f10eb305c95c30ecf480d4c
John <john-francis.edah@tech11.com>
2023-10-10 12:42:47 +0200

    improved create asset page

--- src/app/pages/assets/asset-create/asset-create.component.ts
+++ src/app/pages/assets/asset-create/asset-create.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ListDataModel } from 'src/app/utils/data.models';
+import { MainCategoryList, AssetCategoryList, LiabilityCategoryList } from 'src/app/shared/categories';
+
 
 @Component({
   selector: 'app-asset-create',
@@ -6,5 +10,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./asset-create.component.scss']
 })
 export class AssetCreateComponent {
+  constructor() { }
+
+  assetCreateForm = new FormGroup({
+    accountName: new FormControl<string>('', Validators.required), 
+    categoryName: new FormControl<string>('', Validators.required),
+    isUrgent: new FormControl<string | null>(''),
+  });
+
+  createTypeList: ListDataModel[] = MainCategoryList;
+  assetCategoryList: ListDataModel[] = AssetCategoryList; 
+  liabilityTypeList: ListDataModel[] = LiabilityCategoryList;
+
+/*   setCategoryList(mainCategory: any) {
+    if(mainCategory === 'asset') {
+      return this.assetTypeList;
+    }if(mainCategory === 'liability') {
+      return this.liabilityTypeList;
+    }else {
+      return [];
+    }
+  } */
+  onClickCancel() {
+    history.back()
+  }
+
+  onClickSave() {
+    alert('Tapped save button!')
+  }
 
 }
